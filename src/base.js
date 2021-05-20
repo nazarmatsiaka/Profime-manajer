abee098c1b387751e6191da5b6d31408f1ee48c5
Nazar <nazarchukk06@gmail.com>
2021-05-20 23:10:45 +0300

    implemented firebase hosting

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBh0ksi6wxwD0wxhr8XXXUopYnb2L-NmU4",
+    authDomain: "profile-manager-7313e.firebaseapp.com",
+    projectId: "profile-manager-7313e",
+    storageBucket: "profile-manager-7313e.appspot.com",
+    messagingSenderId: "1075978956658",
+    appId: "1:1075978956658:web:79a0b757e5f40b2414f7eb"
+});
+
+export default firebaseApp;
